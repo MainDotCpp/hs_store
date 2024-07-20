adb16a1d739b37db4bbd85a9ff202ce9a454b54a
yangyang <1400002030@qq.com>
2024-07-20 17:44:53 +0800

    feat: 对接支付功能

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {Toaster} from "@/components/ui/toaster";
-import {Header} from "@/app/header";
+import {Header} from "@/app/components/header";
 
 const inter = Inter({subsets: ["latin"]});
 
