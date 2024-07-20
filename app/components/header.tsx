9b27ccf7801635ab64b1c48d47caac6b7eb7c555
yangyang <1400002030@qq.com>
2024-07-20 18:24:24 +0800

    feat: 对接支付功能

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,19 +1,34 @@
 'use client'
-import {BsBagFill, BsSearch} from "react-icons/bs";
+
+import {usePathname} from "next/navigation";
 import Link from "next/link";
-import {router} from "next/client";
+import {BsBagFill, BsSearch} from "react-icons/bs";
+import {useState} from "react";
 
 export function Header() {
-    let defaultClass = `flex gap-2 items-center cursor-pointer hover:bg-blue-500/25 hover:text-blue-500 hover:scale-105 transition-all h-full px-2`;
-    let activeClass = `flex gap-2 items-center cursor-pointer bg-blue-500/25 text-blue-500 hover:scale-105 transition-all h-full px-2`;
+    let defaultClass = `flex gap-2 items-center cursor-pointer hover:bg-black/10  hover:scale-105 transition-all h-full px-2 h-full rounded-md text-black/85`;
+    let activeClass = `scale-105 bg-black/10`;
+    // 获取当前请求路径
+    let pathname = usePathname();
+    const [menu, setMenu] = useState([
+        {icon: BsBagFill, text: '首页', path: '/'},
+        {icon: BsSearch, text: '订单查询', path: '/query'},
+    ]);
+
     return <>
         <div className="header w-screen h-14 bg-white fixed top-0 opacity-95">
             <div className="flex items-center h-full px-8 text-xl gap-2 text-black/85">
                 <div className="icon"></div>
-                <Link href='/'
-                      className={router.basePath === '/' ? activeClass : defaultClass}><BsBagFill/><span>首页</span></Link>
-                <Link href='/query'
-                      className={router.basePath === '/query' ? activeClass : defaultClass}><BsSearch/><span>订单查询</span></Link>
+                <div className="menu flex gap-2 h-full py-1">
+                    {menu.map((item, index) => (
+                        <Link key={index} href={item.path}>
+                            <div className={`${defaultClass} ${pathname === item.path ? activeClass : ''}`}>
+                                <item.icon/>
+                                <span>{item.text}</span>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </div>
         <div className="h-14"></div>
